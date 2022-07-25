1d400c1d89d653527f24b80b3518010a6c57a231
Bram van den Bogaard <bram.van.den.bogaard@infocaster.net>
2022-07-25 22:12:48 +0200

    initial commit

---
+++ README-NUGET.md
@@ -0,0 +1,5 @@
+# UrlTracker GlobalBlocklist extension
+
+
+
+Want to know more about the awesome packages that we make at Infocaster? Check us out on [GitHub](https://github.com/Infocaster)
---
+++ src/UrlTracker.GlobalBlocklist/Composers/GlobalBlocklistComposer.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace UrlTracker.GlobalBlocklist.Composers
+{
+    public class GlobalBlocklistComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+        }
+    }
+}
