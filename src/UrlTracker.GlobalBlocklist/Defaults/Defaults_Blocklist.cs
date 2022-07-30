20ff1a806678b0200d0f8ccd856728f345a8b07a
Bram van den Bogaard <bram.van.den.bogaard@infocaster.net>
2022-07-30 18:57:32 +0200

    use github url instead of test

---
+++ src/UrlTracker.GlobalBlocklist/Defaults/Defaults_Blocklist.cs
@@ -0,0 +1,11 @@
+﻿namespace UrlTracker.GlobalBlocklist
+{
+    public static partial class Defaults
+    {
+        public static class Blocklist
+        {
+            public const string BlocklistUrl = "https://github.com/Infocaster/.github/blob/main/UrlTracker_globalSettings.json";
+        }
+    }
+    
+}
--- src/UrlTracker.GlobalBlocklist/Services/RetreiveBlocklistService.cs
+++ src/UrlTracker.GlobalBlocklist/Services/RetreiveBlocklistService.cs
@@ -17,7 +17,7 @@ namespace UrlTracker.GlobalBlocklist.Services
         public async Task<GlobalSettings> GetGlobalSettings()
         {
             var _client = _httpClientFactory.CreateClient();
-            var result = await _client.GetAsync("https://1ba6015a-0499-447e-bbc2-e23154d4a14d.mock.pstmn.io/getBlocklistMock");
+            var result = await _client.GetAsync(Defaults.Blocklist.BlocklistUrl);
             result.EnsureSuccessStatusCode();
             var content = await result.Content.ReadAsStringAsync();
 
